f3d306b2657b5ce93292f21403a521279ea3c815
cevatarmutlu <cevatarmutlu@gmail.com>
2021-04-16 21:38:57 +0300

    add: DBFactory (test, log, doc)

---
+++ DBFactory.py
@@ -0,0 +1,35 @@
+#### Project Scripts ####
+from db.DBEnum import DBEnum
+from db.IDB import IDB
+from db.ElasticSearch import ElasticSearch
+from db.PostgreSQL import PostgreSQL
+
+class DBFactory:
+    """
+        Return specific database instance.
+        Examples:
+            DBFactory.getDB(DBEnum.ElasticSearch)
+    """
+
+    @staticmethod
+    def getDB(dbType: DBEnum):
+        """
+            Return specific database instance.
+            Args:
+                dbType: Database type.
+            Returns:
+                IDB subclass: Your DB instance.
+            
+            Raises:
+                TypeError: if `dbType` is not instance of `DBEnum` raises `TypeError`.
+                     
+        """
+
+        if not isinstance(dbType, DBEnum):
+            raise TypeError(f'dbType must be DBEnum type, not {type(filename).__name__}')
+                
+
+        if (dbType == DBEnum.PostgreSQL):
+            return PostgreSQL()
+        elif (dbType == DBEnum.ElasticSearch):
+            return ElasticSearch()
---
+++ tests/TestDBFactory.py
@@ -0,0 +1,31 @@
+# In-Python module
+import unittest
+
+#### Project Scripts ####
+from DBFactory import DBFactory
+from db.DBEnum import DBEnum
+from db.IDB import IDB
+
+class TestDBFactory(unittest.TestCase):
+
+    class_name = "DBFactory"
+
+    def test1_getDB(self):
+
+        print(f'\n**Start {TestDBFactory.class_name} getDB() test**\n')
+
+        #### Invalid ####
+        with self.assertRaises(TypeError):
+            DBFactory.getDB(dBType=5)
+            DBFactory.getDB(dBType='')
+            DBFactory.getDB(dBType='Hello World!')
+            DBFactory.getDB(dBType='MongoDB')
+
+
+        #### Valid ####
+        self.assertIsInstance(
+            DBFactory.getDB(DBEnum.PostgreSQL),
+            IDB
+        )
+        
+        print(f'\n**End {TestDBFactory.class_name} getDB() test**\n')
